b3ad5eef0011d19e5910ad65a72183e3172d541c
Gosha <kovalenko@giria.io>
2023-08-19 22:04:02 +0200

    Add some validation for signup

--- src/signup/service/signup.service.ts
+++ src/signup/service/signup.service.ts
@@ -8,12 +8,16 @@ export class SignupService {
     constructor(private usersServiceDb: UsersServiceDb) {}
 
     async signup(user: SignupDto) {
+        user.username = user.username.replace(/ /g, "");
+        user.email = user.email.replace(/ /g, "");
+        user.password = user.password.replace(/ /g, "");
+
         const userInDb = await this.usersServiceDb.getUserByUsernameOrEmail(user.username, user.email);
 
-        if(userInDb.length && userInDb[0].email === user.email) {
+        if(userInDb.length && userInDb[0].email.toLowerCase() === user.email.toLowerCase()) {
             throw new BadRequestException({ message: "User with this email already exist" });
         }
-        if(userInDb.length && userInDb[0].username === user.username) {
+        if(userInDb.length && userInDb[0].username.toLowerCase() === user.username.toLowerCase()) {
             throw new BadRequestException({ message: "User with this username already exist" });
         }
         const hash = await argon.hash(user.password);
